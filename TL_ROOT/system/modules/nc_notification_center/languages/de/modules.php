0ec52243a8d64a518127a7c951180ac4f9daba16
Marcel Mathias Nolte <marcel.nolte@noltecomputer.de>
2015-09-14 23:03:22 +0200

    no message

--- TL_ROOT/system/modules/nc_notification_center/languages/de/modules.php
+++ TL_ROOT/system/modules/nc_notification_center/languages/de/modules.php
@@ -7,7 +7,7 @@
  * 
  * @package   NC Notification Center
  * @author    Marcel Mathias Nolte
- * @copyright Marcel Mathias Nolte 2013
+ * @copyright Marcel Mathias Nolte 2015
  * @website	  https://www.noltecomputer.com
  * @license   <marcel.nolte@noltecomputer.de> wrote this file. As long as you retain this notice you
  *            can do whatever you want with this stuff. If we meet some day, and you think this stuff 
